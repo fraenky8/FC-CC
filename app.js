21973c07f0fa13df23dd7e6b0e87bde22c37b306
Frank Meyer <fr4nk235@gmail.com>
2016-09-23 09:21:53 +0200

    removed stuff dont needed here

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -10,22 +8,14 @@ var error404Handler = require('./middlewares/errorhandler/error404');
 var devErrorHandler = require('./middlewares/errorhandler/developmentErrorHandler');
 var prodErrorHandler = require('./middlewares/errorhandler/productionErrorHandler');
 
-var restApi = require('./routes/api');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+var restApi = require('./routes/api');
 app.use('/', restApi);
 
 // catch 404 and forward to error handler
